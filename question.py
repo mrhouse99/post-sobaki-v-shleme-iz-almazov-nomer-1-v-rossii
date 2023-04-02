9a04546b8a22dfe2d2e998911c6e0fc1c8c60b06
mrhouse99 <129657787+mrhouse99@users.noreply.github.com>
2023-04-02 15:14:15 +0500

    Add files via upload

---
+++ question.py
@@ -0,0 +1,7 @@
+class Question():
+    def __init__ (self, args):
+        self.que = args[0]
+        self.rightans = args[1]
+        self.wrong_ans1 = args[2]
+        self.wrong_ans2 = args[3]
+        self.wrong_ans3 = args[4]
